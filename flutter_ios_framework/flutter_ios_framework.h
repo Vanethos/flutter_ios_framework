afb86f79fd16d9076694dd7cf3671869345582ba
Gonçalo Palma <solid.goncalo@gmail.com>
2019-07-08 12:11:50 +0100

    Initial Commit

---
+++ flutter_ios_framework/flutter_ios_framework.h
@@ -0,0 +1,19 @@
+//
+//  flutter_ios_framework.h
+//  flutter_ios_framework
+//
+//  Created by Gonçalo Montes Palma on 08/07/2019.
+//  Copyright © 2019 Gonçalo Montes Palma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for flutter_ios_framework.
+FOUNDATION_EXPORT double flutter_ios_frameworkVersionNumber;
+
+//! Project version string for flutter_ios_framework.
+FOUNDATION_EXPORT const unsigned char flutter_ios_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <flutter_ios_framework/PublicHeader.h>
+
+
